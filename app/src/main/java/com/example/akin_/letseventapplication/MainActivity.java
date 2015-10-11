3237c7f397c81ccd12583311ad2591334ddae7a3
Omer Akin <akin__08@hotmail.com>
2015-10-11 12:53:44 +0300

    third commit

--- app/src/main/java/com/example/akin_/letseventapplication/MainActivity.java
+++ app/src/main/java/com/example/akin_/letseventapplication/MainActivity.java
@@ -17,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        String omer = "";
+        String yusuf = "Hadi yusuf hadiii";
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
