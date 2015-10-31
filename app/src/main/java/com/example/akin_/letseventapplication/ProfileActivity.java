c09bae101a0b7910a223020d308659422554a89d
Omer Akin <akin__08@hotmail.com>
2015-10-31 16:02:28 +0200

    SomeClasses+Parse

--- app/src/main/java/com/example/akin_/letseventapplication/ProfileActivity.java
+++ app/src/main/java/com/example/akin_/letseventapplication/ProfileActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class ProfileActivity extends AppCompatActivity {
 //comment
+    //alındı
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
