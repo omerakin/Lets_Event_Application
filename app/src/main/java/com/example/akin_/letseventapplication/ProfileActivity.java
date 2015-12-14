e168d4408c19fc5265b538a205f5dbdf98129b98
Omer Akin <akin__08@hotmail.com>
2015-12-14 15:36:33 +0200

    Facebook is coming...

--- app/src/main/java/com/example/akin_/letseventapplication/ProfileActivity.java
+++ app/src/main/java/com/example/akin_/letseventapplication/ProfileActivity.java
@@ -4,10 +4,26 @@ import android.app.LocalActivityManager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TabHost;
 import android.widget.TextView;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    TextView NameLastname;
+    String UsernameString;
+    String PasswordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +58,92 @@ public class ProfileActivity extends AppCompatActivity {
         EventsToAttendTextView.setTextSize(9);
         TextView pastEventsTextView = (TextView) tabHost3.getTabWidget().getChildAt(2).findViewById(android.R.id.title);
         pastEventsTextView.setTextSize(10);
+
+
+        setNameAndLastnameOfUser();
+        getNameandLastnameFromParse();
+
+
+    }
+
+    private void setNameAndLastnameOfUser() {
+
+        // Read Username from file
+        FileInputStream fisUsername = null;
+        try {
+            fisUsername = openFileInput("UsernameInfromation.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        BufferedInputStream bisUsername = new BufferedInputStream(fisUsername);
+        StringBuffer Username = new StringBuffer();
+        try {
+            while (bisUsername.available() != 0){
+                char next = (char) bisUsername.read();
+                Username.append(next);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        UsernameString = Username.toString();
+
+        // Read Password from file
+        FileInputStream fisPassword = null;
+        try {
+            fisPassword = openFileInput("PasswordInfromation.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        BufferedInputStream bisPassword = new BufferedInputStream(fisPassword);
+        StringBuffer Password = new StringBuffer();
+        try {
+            while (bisPassword.available() != 0){
+                char next = (char) bisPassword.read();
+                Password.append(next);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        PasswordString = Password.toString();
+    }
+
+    private void getNameandLastnameFromParse() {
+
+        //NameLastname = (TextView) findViewById(R.id.nameLastname);
+        NameLastname = (TextView) findViewById(R.id.textView4);
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("TestAccount");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> parseAccounts, ParseException e) {
+                if (e == null) {
+                    int len = parseAccounts.size();
+                    for (int i = 0; i < len; i++) {
+                        //get the i th element of Accounts in order to obtain details
+                        ParseObject p = parseAccounts.get(i);
+
+                        //get the detail information of userAccount
+                        String pEmail = p.getString("Email");
+                        String pPassword = p.getString("Password");
+                        String pName;
+                        String pLastname;
+
+                        if(pEmail.equals(UsernameString) && pPassword.equals(PasswordString)){
+                            pName = p.getString("Name");
+                            pLastname = p.getString("Lastname");
+                            NameLastname.setText(pName + " " + pLastname);
+                            return;
+                        } else {
+                            NameLastname.setText("Name Lastname");
+                        }
+
+                    }
+                } else {
+                    System.out.println("Error::: in getNameandLastnameFromParse()!");
+
+                }
+            }
+        });
+
     }
 
     @Override
